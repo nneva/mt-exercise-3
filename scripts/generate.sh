20b6b5ecbdde55f6a39dc11fb0332f7319135a34
Nevena Nikolic <nevena.nikolic@uzh.ch>
2023-03-01 17:11:41 +0100

    Add input flag

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,7 +5,7 @@ base=$(realpath $scripts/..)
 
 models=$base/models
 data=$base/data
-tools=$base/tools
+examples=$base/examples
 samples=$base/samples
 
 mkdir -p $samples
@@ -13,10 +13,12 @@ mkdir -p $samples
 num_threads=4
 device=""
 
-(cd $tools/pytorch-examples/word_language_model &&
+(cd $examples/word_language_model &&
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
         --data $data/dorian \
         --words 500 \
         --checkpoint $models/model_0_35.pt \
-        --outf $samples/sample.txt
+        --outf $samples/sample.txt \
+        --temperature 0.6 \
+        --input True
 )
